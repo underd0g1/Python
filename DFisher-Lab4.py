76fb9716a7e15b78dcf8a386e45a4ac348d47fe7
underd0g1 <hide4@comcast.net>
2016-03-03 18:29:32 -0500

    lab 4

---
+++ DFisher-Lab4.py
@@ -0,0 +1,14 @@
+
+ #*************************************     #      Lab4                          *     #      Author: DANE FISHER           *     #*************************************     #This Programm Prints My Initials DF.     
+print(‘DD’, ‘DDD’,”               “,’FFFFFFFFFFFF’)
+print(‘DD’ , “   ”    ’DDD’, “    “,’FFFFFFFFFFFF’)
+print(‘DD’, “      ”  ‘DDDD’,”    “,’FFF’)
+print(‘DD’,”           “ ‘DDDD’,” “,’FFF’)
+print(‘DD’,”           “ ‘DDDD’,” “,’FFFFFFFFFF’)
+print(‘DD’,”            “ ‘DDDD’,”“,’FFFFFFFFFF’)
+print(‘DD’,”            “ ’DDDD’,”“,’FF’)
+print(‘DD’,“            “ ‘DDDD’,”“,’FF’)
+print(‘DD’,“            “ ‘DDDD’,”“,’FF’)
+print(‘DD’,“         “ ‘DDDD’,”   “,’FF’)
+print(‘DD’,“     “ ‘DDDD’,”       “,’FF’)
+print(‘DD’,’DDD’,”                “,’FF’)
